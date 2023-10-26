71db18935cbf393a2e56cfb769a1608ac4bc47da
eduaardofranco <eduaardo.franco@gmail.com>
2023-10-26 18:43:35 +0100

    footer responsive

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,14 +1,18 @@
 import styled from 'styled-components'
+import { DEVICE_BREAKPOINTS } from '../../styles/layoutBreakpoints' 
+
 
 export const Container = styled.div`
     background-color: ${({ theme }) => theme.COLORS.BG_DARK_600};
-    color: ${({ theme }) => theme.COLORS.TXT_GRAY_700};
-    font-family: ${({ theme }) => theme.FONTS.ROBOTO};
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 2.4rem 1rem;
-
+    .content {
+        color: ${({ theme }) => theme.COLORS.TXT_GRAY_700};
+        font-family: ${({ theme }) => theme.FONTS.ROBOTO};
+        padding: 2.4rem 1rem;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    
     div {
         display: flex;
         align-items: center;
