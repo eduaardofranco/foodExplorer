46a28fc9514b5571194054004bb185c157e21361
eduaardofranco <eduaardo.franco@gmail.com>
2023-12-07 18:52:27 +0000

    navigate to login when logout

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -24,7 +24,7 @@ export function Menu({ menuIsOpen, onCloseMenu, onSearch }) {
     function handleSignOut(e) {
         e.preventDefault()
         signOut()
-        navigate('/login')
+        navigate('/')
     }
     
     return(
